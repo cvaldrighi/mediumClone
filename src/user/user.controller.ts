35b831ab2efb158a805fa6dae716fc13de54cca4
cvaldrighi <valdrighdev@gmail.com>
2022-06-27 15:56:23 -0300

    userCreate validation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { UserService } from "@app/user/user.service";
 import { CreateUserDto } from "@app/user/dto/createUser.dto";
 import { userResponseInterface } from "./types/userResponse.interface";
@@ -8,6 +8,7 @@ export class UserController {
     constructor(private readonly userService: UserService) {}
 
     @Post('users')
+    @UsePipes(new ValidationPipe())
     async createUser(@Body('user') createUserDto: CreateUserDto): Promise<userResponseInterface> {
         const user = await this.userService.createUser(createUserDto);
         return this.userService.buildUserResponse(user);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { CreateUserDto } from "@app/user/dto/createUser.dto";
 import { UserEntity } from "./user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
@@ -15,6 +15,20 @@ export class UserService {
     ){}
 
     async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+        const userByEmail = await this.userRepository.findOne({
+            where: {
+                email: createUserDto.email,
+            }          
+        });
+        const userByUsername = await this.userRepository.findOne({
+            where: {
+                username: createUserDto.username,
+            }
+        });
+        if(userByEmail || userByUsername) {
+            throw new HttpException('Email or username are taken', HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
         const newUser = new UserEntity();
         Object.assign(newUser, createUserDto);
         console.log('newUser', newUser);

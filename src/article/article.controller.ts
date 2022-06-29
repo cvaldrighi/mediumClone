dce57957bf7061e2d7115e20fc2e37e008eeb28b
cvaldrighi <valdrighdev@gmail.com>
2022-06-29 12:26:36 -0300

    added get-put-delete methods to article module

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,7 +1,7 @@
 import { User } from "@app/user/decorators/user.decorator";
 import { AuthGuard } from "@app/user/guards/auth.guard";
 import { UserEntity } from "@app/user/user.entity";
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
 import { ArticleService } from "./article.service";
 import { CreateArticleDto } from "./dto/createArticle.dto";
 import { ArticleResponseInterface } from "./types/articleResponse.interface";
@@ -12,11 +12,36 @@ export class ArticleController {
 
     @Post()
     @UseGuards(AuthGuard)
+    @UsePipes(new ValidationPipe())
     async create(
         @User() currentUser: UserEntity, 
         @Body('article') createArticleDto: CreateArticleDto
     ): Promise<ArticleResponseInterface> {
         const article = await this.articleService.createArticle(currentUser, createArticleDto);
+        return this.articleService.buildArticleResponse(article);
+    }
+
+    @Get(':slug')
+    async getSingleArticle(@Param('slug') slug: string): Promise<ArticleResponseInterface> {
+        const article = await this.articleService.findBySlug(slug)
+        return this.articleService.buildArticleResponse(article);
+    }
+
+    @Delete(':slug')
+    @UseGuards(AuthGuard)
+    async deleteArticle(@User('id') currentUserId: number, @Param('slug') slug: string) {
+        return await this.articleService.deleteArticle(slug, currentUserId);
+    }
+
+    @Put(':slug')
+    @UseGuards(AuthGuard)
+    @UsePipes(new ValidationPipe())
+    async updateArticle(
+        @User('id') currentUserId: number,
+        @Param('slug') slug: string,
+        @Body('article') updateArticleDto: CreateArticleDto   
+    ){
+        const article = await this.articleService.updateArticle(slug, updateArticleDto, currentUserId);
         return this.articleService.buildArticleResponse(article);
     }
 }

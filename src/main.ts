d1aa910f1c98db09899f47322595164f520db07e
cvaldrighi <valdrighdev@gmail.com>
2022-07-05 10:35:51 -0300

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-if(!process.env.IS_TS_NODE) {
+if (!process.env.IS_TS_NODE) {
   require('module-alias/register');
 }
 import { NestFactory } from '@nestjs/core';
@@ -6,6 +6,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
